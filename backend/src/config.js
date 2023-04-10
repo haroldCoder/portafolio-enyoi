3fa11d461df58a2b7d066a31f27491c30eec624b
haroldCoder <haroldc2005@gmail.com>
2023-04-09 19:53:02 -0500

    PORT

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,11 +3,13 @@ const DB_USER = process.env.DB_USER || 'root'
 const DB_PASSWORD = process.env.DB_PASSWORD || '2364144'
 const DB_NAME = process.env.DB_NAME || 'request'
 const DB_PORT = process.env.DB_PORT || 3306
+const PORT = process.env.PORT || 4000
 
 module.exports = {
     DB_HOST,
     DB_NAME,
     DB_PASSWORD,
     DB_USER,
-    DB_PORT
+    DB_PORT,
+    PORT
   }
