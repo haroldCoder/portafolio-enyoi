80de88079bac81b7a0a8244e6b17a590df3a7b70
haroldCoder <haroldc2005@gmail.com>
2023-04-09 19:42:57 -0500

    PORT

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require("cors");
-const {PORT} = require("./config")
+const {DB_PORT} = require("./config")
 
 // middleware
 app.use(bodyParser.json());
